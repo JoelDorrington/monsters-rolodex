ee6b0cc4c97addbc1353b7ddaface5f347bf3bd4
Joel Dorrington <joel_dorrington@hotmail.com>
2020-04-11 17:39:13 -0400

    Updates

---
+++ precache-manifest.6735fe0253366f9fbbe3c7cdea8630cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635659280708d0e68f72c0eb9adf8950",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b2a1b579d23a9e73bb45",
+    "url": "/monsters-rolodex/static/css/main.6e0e860d.chunk.css"
+  },
+  {
+    "revision": "9d84f5b306774dfc9f1a",
+    "url": "/monsters-rolodex/static/js/2.91e906d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.91e906d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a1b579d23a9e73bb45",
+    "url": "/monsters-rolodex/static/js/main.3924657c.chunk.js"
+  },
+  {
+    "revision": "3b3d63f18db8e96c0d37",
+    "url": "/monsters-rolodex/static/js/runtime-main.503a8114.js"
+  }
+]);
